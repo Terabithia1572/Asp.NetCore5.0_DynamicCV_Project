a3b16dd430bcab5323916b2fc2197e1fc53a0606
Yunus İNAN <legendys65@gmail.com>
2021-10-31 13:27:37 +0300

    Controller/Added+Updated

---
+++ Asp.NetCore5.0_DynamicCV_Project/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DynamicCV_Project.Controllers
+{
+    public class AdminController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutRepository());
+        public IActionResult AboutIndex()
+        {
+            var values = aboutManager.GetList();
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AboutUpdate(int id)
+        {
+            var values = aboutManager.GetById(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult AboutUpdate(About t)
+        {
+
+            aboutManager.Update(t);
+            return RedirectToAction("Index", "About");
+
+        }
+    }
+}
---
+++ Asp.NetCore5.0_DynamicCV_Project/Controllers/AdminEducationController.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DynamicCV_Project.Controllers
+{
+    public class AdminEducationController : Controller
+    {
+        EducationManager educationManager = new EducationManager(new EfEducationRepository());
+        public IActionResult Index()
+        {
+            var value = educationManager.GetList();
+            return View(value);
+        }
+
+        [HttpGet]
+        public IActionResult EducationUpdate(int id)
+        {
+            var value = educationManager.GetById(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult EducationUpdate(Education t)
+        {
+            educationManager.Update(t);
+            return RedirectToAction("Index", "Education");
+        }
+
+        [HttpGet]
+        public IActionResult AddEducation()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddEducation(Education t)
+        {
+            educationManager.Add(t);
+            return RedirectToAction("Index", "Education");
+        }
+    }
+}
